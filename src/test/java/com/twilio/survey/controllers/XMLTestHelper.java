1562c7d2c03d921c79981ae39cc79366b45c68bb
Mario Celi <mceli@stackbuilders.com>
2016-09-15 11:50:13 -0500

    Apply google style guide

--- src/test/java/com/twilio/survey/controllers/XMLTestHelper.java
+++ src/test/java/com/twilio/survey/controllers/XMLTestHelper.java
@@ -1,22 +1,24 @@
 package com.twilio.survey.controllers;
 
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+import java.io.IOException;
+import java.io.StringReader;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.io.StringReader;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 public class XMLTestHelper {
-    public static Document createDocumentFromXml(String input) throws ParserConfigurationException, SAXException, IOException {
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+  public static Document createDocumentFromXml(String input)
+      throws ParserConfigurationException, SAXException, IOException {
+    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
 
-        StringReader stringReader = new StringReader(input);
-        InputSource inputSource = new InputSource(stringReader);
-        return documentBuilder.parse(inputSource);
-    }
+    StringReader stringReader = new StringReader(input);
+    InputSource inputSource = new InputSource(stringReader);
+    return documentBuilder.parse(inputSource);
+  }
 }
